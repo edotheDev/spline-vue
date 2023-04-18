0bf8f4e0dada46aff09266b8b0ab898f2624035b
Myat Htet <myathtethlaing@kbtc.edu.mm>
2023-04-18 22:59:05 +0630

    18/4/2023

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link>
   </nav>
   <router-view/>
+  <div class="model">
+    <Spline :scene="scene" />
+  </div>
 </template>
 
 <style>
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,7 +32,30 @@ nav a {
   color: #2c3e50;
 }
 
+.model{
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
 nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+
+<script>
+import { defineComponent } from 'vue';
+import Spline from "spline-vue/v3";
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Spline
+  },
+  data: () => ({
+    scene: "https://prod.spline.design/2XVCWOkcGNZH0hue/scene.splinecode",
+  })
+});
+</script>
---
+++ src/components/buildingComponent.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Spline from 'vue-spline';
+</script>
+
+<template>
+  <div>Hello</div>
+  <Spline scene="https://prod.spline.design/2XVCWOkcGNZH0hue/scene.splinecode" />
+</template>
+
+
+<style scoped>
+
+</style>
+
+<script>
+export default {
+  name: "buildingComponent",
+}
+</script>
